2a9e65ad73426bdafe66f2a75aaf15d771c8de03
saveliyb <77884647+saveliyb@users.noreply.github.com>
2021-11-03 17:27:34 +0500

    Add files via upload

--- maint.py
+++ maint.py
@@ -1,5 +1,6 @@
 from pprint import pprint
 import math
+import numpy as np
 
 def set_many_list(lst_points):
     lst = [_ for _ in lst_points]
@@ -24,6 +25,33 @@ def set_many_list(lst_points):
         answer.append([(_, dct[_]) for _ in dct.keys()])
         dct.clear()
     return answer
+
+def set_list_x_y(lst):
+    x_lst = []
+    y_lst = []
+    x_lst_ = []
+    y_lst_ = []
+    for elem in lst:
+        # print(elem)
+        for e_ in elem:
+            # print(e_)
+            for e in e_:
+                # print(e)
+                # print(e)
+                x_lst.append(e[0])
+                y_lst.append(-e[1])
+            x_lst_.append(x_lst)
+            y_lst_.append(y_lst)
+            # print('xxxx', x_lst, y_lst)
+            x_lst = []
+            y_lst = []
+        # print(x_lst, y_lst)
+    # print('---')
+    # pprint((np.asarray(x_lst), np.asarray(y_lst)))
+    # print(x_lst_)
+    # print(y_lst_)
+    # print('------------------------')
+    return (x_lst_, y_lst_)
 
 
 abc = [(_, int(math.sin(_/10)* 100)) for _ in range(-156, 360)]
